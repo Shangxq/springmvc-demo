24496726d64e6243a28c84b73418059b358fb382
尚晓琼 <1194254278@qq.com>
2017-12-29 14:06:08 +0800

    spring mvc 测试单元

---
+++ src/main/java/com/shang/learn/springmvc/service/DemoService.java
@@ -0,0 +1,10 @@
+package com.shang.learn.springmvc.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoService {
+    public String satSomething(){
+        return "尚晓琼";
+    }
+}
